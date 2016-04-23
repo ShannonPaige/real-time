8d1fc6494a3176f51360c42806587e69124ab971
ShannonPaige <spaige@gmail.com>
2016-04-23 15:41:10 -0600

    Set up jade and view folder

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const express = require('express');
-
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'jade');
 
-app.get('/', function (req, res){
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/', function (request, response){
+  response.render('index');
 });
 
 const port = process.env.PORT || 3000;
@@ -18,5 +18,13 @@ const server = http.createServer(app)
 
 const socketIo = require('socket.io');
 const io = socketIo(server);
-                
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+  });
+});
+
 module.exports = server;
