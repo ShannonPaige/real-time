1ffcaea1f63488984731a4e60c78501583273915
ShannonPaige <spaige@gmail.com>
2016-04-23 16:32:03 -0600

    Set up bootstrap

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app)
+                    .listen(port, function () {
+                      console.log('Listening on port ' + port + '.');
+                    });
+const socketIo = require('socket.io');
+const io = socketIo(server);
 
-app.use(express.static('public'));
 app.set('view engine', 'jade');
 
+app.use(express.static('public'));
+
+app.locals.title = 'Crowdsource';
+
+
+/* Routes */
 app.get('/', function (request, response){
   response.render('index');
 });
 
-const port = process.env.PORT || 3000;
 
-const server = http.createServer(app)
-                 .listen(port, function () {
-                    console.log('Listening on port ' + port + '.');
-                  });
-
-const socketIo = require('socket.io');
-const io = socketIo(server);
-
+/* Sockets */
 io.on('connection', function (socket) {
   console.log('A user has connected.', io.engine.clientsCount);
 
