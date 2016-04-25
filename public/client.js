53b4a31489b37e8abafd08df5d0d39640d6d120e
ShannonPaige <spaige@gmail.com>
2016-04-25 10:02:14 -0600

    Add helper function to append a div

--- public/client.js
+++ public/client.js
@@ -1,13 +1,21 @@
-var socket = io();
+var socket            = io();
 
-var buttons = document.querySelectorAll('#choices button');
+var addOption         = document.getElementById('add_option');
+var buttons           = document.querySelectorAll('#choices button');
+var closePoll         = document.getElementById('close_poll');
+var options           = document.getElementById('options');
+var pollStatus        = document.getElementById('poll_status');
+var voteConfirmation  = document.getElementById('vote-confirmation');
+var votesCount        = document.getElementById('votes_count');
+var votesShare        = document.getElementById('votes_share');
+
+/* EVENT LISTENERS */
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
     socket.send('voteCast', {votingId: this.className, voteContent: this.innerText});
   });
 }
 
-var addOption = document.getElementById('add_option');
 if (!!addOption){
   addOption.addEventListener('click', function (e) {
     e.preventDefault();
@@ -15,46 +23,44 @@ if (!!addOption){
   });
 }
 
-var options = document.getElementById('options');
-socket.on('addAnotherOption', function () {
-  options.innerHTML = options.innerHTML + 'Option:<input type="text" name="poll_options[]"><br />';
-});
-
-var voteConfirmation = document.getElementById('vote-confirmation');
-socket.on('voteConfirmation', function (vote) {
-  voteConfirmation.innerText = 'Your vote for "' + vote + '" has been counted.';
-});
-
-var votesCount = document.getElementById('votes_count');
-socket.on('voteCount', function (votingInfo) {
-  if (votingInfo.dashboardId === votesCount.className){
-    var votingResults = '';
-    for (var vote in votingInfo.votes) {
-      votingResults += vote + ': ' + votingInfo.votes[vote] + '\n\n';
-    }
-    votesCount.innerText = votingResults;
-  }
-});
-
-var votesShare = document.getElementById('votes_share');
-socket.on('voteShare', function (votingInfo) {
-  if (votingInfo.votingId === votesShare.className){
-    var votingResults = '';
-    for (var vote in votingInfo.votes) {
-      votingResults += vote + ': ' + votingInfo.votes[vote] + '\n\n';
-    }
-    votesShare.innerText = votingResults;
-  }
-});
-
-var closePoll = document.getElementById('close_poll');
 if (!!closePoll){
   closePoll.addEventListener('click', function () {
     socket.send('closePoll', {votingId: this.className});
   });
 }
 
-var pollStatus = document.getElementById('poll_status');
+
+/* SOCKETS */
+socket.on('addAnotherOption', function () {
+  options.innerHTML = options.innerHTML + 'Option:<input type="text" name="poll_options[]"><br />';
+});
+
+socket.on('voteConfirmation', function (vote) {
+  voteConfirmation.innerText = 'Your vote for "' + vote + '" has been counted.';
+});
+
+socket.on('voteCount', function (votingInfo) {
+  if (!!votesCount && votingInfo.dashboardId === votesCount.className){
+    appendCurrentPoll(votingInfo, votesCount);
+  }
+});
+
+socket.on('voteShare', function (votingInfo) {
+  if (!!votesShare && votingInfo.votingId === votesShare.className){
+    appendCurrentPoll(votingInfo, votesShare);
+  }
+});
+
 socket.on('pollStatus', function () {
   pollStatus.innerText = 'This poll has been closed for voting';
 });
+
+
+/* HELPERS */
+function appendCurrentPoll(votingInfo, div) {
+  var votingResults = '';
+  for (var vote in votingInfo.votes) {
+    votingResults += vote + ': ' + votingInfo.votes[vote] + '\n\n';
+  }
+  div.innerText = votingResults;
+}
